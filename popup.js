cca07e9ae67230e0a96be0a6b1d7a48fabaa34c5
offbrandd <btytler411@gmail.com>
2023-07-14 15:18:12 -0700

    1.10 can now select between .csv and .xlsx improved user process flow

--- popup.js
+++ popup.js
@@ -1,18 +1,21 @@
-console.log("This is a popup!")
-getCurrentURL()
-
 function modifyURL(url) {
     start = url.indexOf("/edit")
     end = url.indexOf("=")
-    console.log(start)
-    console.log(end)
     badText = url.substring(start, end+1)
-    goodText = "/export?format=xlsx&gid="
+
+    format = document.getElementById('format').value;
+    if (format == "csv") {
+        goodText = "/export?format=csv&gid="
+    }
+    else if (format == "xlsx") {
+        goodText = "/export?format=xlsx&gid="
+    }
     url = url.replace(badText, goodText)
     return url
 
 }
 async function getCurrentURL() {
+    
     let queryOptions = { active: true, lastFocusedWindow: true };
     chrome.tabs.query(queryOptions, function(tabs){
         console.log(tabs[0].url)
@@ -20,4 +23,29 @@ async function getCurrentURL() {
         chrome.tabs.update({url: updatedUrl});
         return tabs[0].url;
     });
+    window.close()
   }
+
+  // Saves options to chrome.storage
+const saveOptions = () => {
+    const format = document.getElementById('format').value;
+  
+    chrome.storage.sync.set(
+      { fileFormat: format }
+    );
+  };
+  
+  // Restores select box and checkbox state using the preferences
+  // stored in chrome.storage.
+  const restoreOptions = () => {
+    chrome.storage.sync.get(
+      { fileFormat: 'csv'},
+      (items) => {
+        document.getElementById('format').value = items.fileFormat;
+      }
+    );
+  };
+  
+  document.addEventListener('DOMContentLoaded', restoreOptions);
+  document.getElementById('format').addEventListener('change', saveOptions)
+  document.getElementById('download').addEventListener('click', function(){getCurrentURL(); })
